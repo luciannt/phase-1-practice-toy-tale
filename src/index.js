5b88c9ff8bea6ef129b38bb56a3cb7887c11cbf5
Lucianna Tittle <lucianna.roset@gmail.com>
2022-08-16 10:05:01 -0500

    Created toyMaker function for new cards

--- src/index.js
+++ src/index.js
@@ -13,3 +13,37 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function toyMaker(toys) {
+  toys.forEach((toy) => {
+    const collection = document.getElementById("toy-collection");
+
+    const toyCard = document.createElement("div");
+    const toyHeader = document.createElement("h2");
+    const toyImage = document.createElement("img");
+    const toyText = document.createElement("p");
+    const toyButton = document.createElement("button");
+
+    toyCard.className = "card";
+    toyButton.className = "like-btn";
+
+    toyImage.src = toy.image;
+    toyButton.id = toy.id;
+
+    toyHeader.textContent = "[input-text]";
+    toyText.textContent = toy[likes] + " " + "Likes";
+    toyButton.textContent = "Like ❤️";
+
+    collection.appendChild(toyCard);
+    toyCard.appendChild(toyHeader);
+    toyCard.appendChild(toyImage);
+    toyCard.appendChild(toyText);
+    toyCard.appendChild(toyButton);
+  });
+}
+
+fetch("http://localhost:3000/toys")
+  .then((res) => res.json)
+  .then((data) => {
+    console.log(data);
+  });
